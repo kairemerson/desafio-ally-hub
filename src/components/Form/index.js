84e4d575e8d12adcfe3bcc4df0188f309dc389d9
kairemerson <kairemerson@gmail.com>
2022-10-28 21:10:27 -0300

    adicionado componente info e finalizado projeto

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -6,11 +6,14 @@ import {useForm} from "react-hook-form"
 import {yupResolver} from "@hookform/resolvers/yup"
 import * as yup from "yup"
 import { validateCpf, validatePhone } from "../../utils/validations"
+import Info from "../Info"
 
 export default props =>{
 
     const [countries, setCountries] = useState([])
     const [cities, setCities] = useState([])
+    const [openInfo, setOpenInfo] = useState(false)
+    const [dataInfo, setDataInfo] = useState({})
 
     const validationSchema = yup.object({
         name: yup.string().required("O campo nome é obrigatório"),
@@ -30,9 +33,13 @@ export default props =>{
             cpf: ""
         }
     })
+    const closeInfo = ()=>{
+        setOpenInfo(false)
+    }
 
     const onSubmit = (data) =>{
-        console.log(data)
+        setOpenInfo(true)
+        setDataInfo(data)
         reset()
     }
 
@@ -52,55 +59,59 @@ export default props =>{
     },[])
 
     return(
-        <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
-            <div className={styles.wrapper}>
-                <div className={styles.formWrapper}>
-                    <h2 className={styles.title}>Dados Pessoais</h2>
-                        <div className={styles.formGroup}>
-                            <label className={styles.labelGroup}>*Nome: </label>
-                            <input className={styles.inputGroup} type="text" name="name" {...register("name")} placeholder="Insira seu nome"/>
-                            <p className={styles.errorMessage}>{errors.name?.message}</p>
-                        </div>
-                        <div className={styles.formGroup}>
-                            <label className={styles.labelGroup}>*Email: </label>
-                            <input className={styles.inputGroup} type="text" name="email" {...register("email")} placeholder="Insira um email válido"/>
-                            <p className={styles.errorMessage}>{errors.email?.message}</p>
-                        </div>
-                        <div className={styles.formGroup}>
-                            <label className={styles.labelGroup}>*Telefone: </label>
-                            <input className={styles.inputGroup} type="text" name="phone" {...register("phone")} placeholder="Insira o seu celular ex:(99)99999-9999"/>
-                            <p className={styles.errorMessage}>{errors.phone?.message}</p>
-                        </div>
-                        <div className={styles.formGroup}>
-                            <label className={styles.labelGroup}>*CPF: </label>
-                            <input className={styles.inputGroup} type="text" name="cpf" {...register("cpf")} placeholder="Insira seu CPF ex:000.000.000-00"/>
-                            <p className={styles.errorMessage}>{errors.cpf?.message}</p>
-                        </div>
-                </div>
-                <div className={styles.formWrapper}>
-                    <h2 className={styles.title}>Destinos de Interesse</h2>
-                    <p>Segure ctrl para selecionar mais de uma opção.</p>
-                        <div className={styles.formGroup}>
-                            <label className={styles.labelGroup}>*País: </label>
-                            <select className={styles.selectGroup} multiple {...register("country")}>
-                                {countries.map((country)=>(
-                                    <option key={country.code} value={country.name}>{country.name_ptbr}</option>
+        <>
+            {!openInfo ? <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+                <div className={styles.wrapper}>
+                    <div className={styles.formWrapper}>
+                        <h2 className={styles.title}>Dados Pessoais</h2>
+                            <div className={styles.formGroup}>
+                                <label className={styles.labelGroup}>*Nome: </label>
+                                <input className={styles.inputGroup} type="text" name="name" {...register("name")} placeholder="Insira seu nome"/>
+                                <p className={styles.errorMessage}>{errors.name?.message}</p>
+                            </div>
+                            <div className={styles.formGroup}>
+                                <label className={styles.labelGroup}>*Email: </label>
+                                <input className={styles.inputGroup} type="text" name="email" {...register("email")} placeholder="Insira um email válido"/>
+                                <p className={styles.errorMessage}>{errors.email?.message}</p>
+                            </div>
+                            <div className={styles.formGroup}>
+                                <label className={styles.labelGroup}>*Telefone: </label>
+                                <input className={styles.inputGroup} type="text" name="phone" {...register("phone")} placeholder="Insira o seu celular ex:(99)99999-9999"/>
+                                <p className={styles.errorMessage}>{errors.phone?.message}</p>
+                            </div>
+                            <div className={styles.formGroup}>
+                                <label className={styles.labelGroup}>*CPF: </label>
+                                <input className={styles.inputGroup} type="text" name="cpf" {...register("cpf")} placeholder="Insira seu CPF ex:000.000.000-00"/>
+                                <p className={styles.errorMessage}>{errors.cpf?.message}</p>
+                            </div>
+                    </div>
+                    <div className={styles.formWrapper}>
+                        <h2 className={styles.title}>Destinos de Interesse</h2>
+                        <p>Segure ctrl para selecionar mais de uma opção.</p>
+                            <div className={styles.formGroup}>
+                                <label className={styles.labelGroup}>*País: </label>
+                                <select className={styles.selectGroup} multiple {...register("country")}>
+                                    {countries.map((country)=>(
+                                        <option key={country.code} value={country.name}>{country.name_ptbr}</option>
+                                        ))}
+                                </select>
+                                <p className={styles.errorMessage}>{errors.country?.message}</p>
+                            </div>
+                            <div className={styles.formGroup}>
+                                <label className={styles.labelGroup}>*Cidade: </label>
+                                <select className={styles.selectGroup} {...register("city")} multiple>
+                                    {cities.map((city)=>(
+                                        <option key={city.id} value={city.name}>{city.name_ptbr}</option>
                                     ))}
-                            </select>
-                            <p className={styles.errorMessage}>{errors.country?.message}</p>
-                        </div>
-                        <div className={styles.formGroup}>
-                            <label className={styles.labelGroup}>*Cidade: </label>
-                            <select className={styles.selectGroup} {...register("city")} multiple>
-                                {cities.map((city)=>(
-                                    <option key={city.id} value={city.name}>{city.name_ptbr}</option>
-                                ))}
-                            </select>
-                            <p className={styles.errorMessage}>{errors.city?.message}</p>
-                        </div>
+                                </select>
+                                <p className={styles.errorMessage}>{errors.city?.message}</p>
+                            </div>
+                    </div>
                 </div>
-            </div>
-            <button className={styles.send} type="submit">Enviar</button>
-        </form>
+                <button className={styles.send} type="submit">Enviar</button>
+            </form>
+            : <Info data={dataInfo} close={closeInfo}></Info>}
+        </>
+        
     )
 }
--- src/components/Info/index.js
+++ src/components/Info/index.js
@@ -1,5 +1,28 @@
-export default props=>(
-    <div>
-        info
-    </div>
-)
+import styles from "./info.module.css"
+
+export default props=>{
+
+    const {name, email, phone, cpf, country, city} = props.data
+    return(
+        <div className={styles.wrapper}>
+            <button className={styles.close} onClick={()=>props.close()}>X</button>
+            <h2>Dados informados</h2>
+            <h3 className={styles.title}>Nome</h3>
+            <p className={styles.text}>{name}</p>
+            <h3 className={styles.title}>Email</h3>
+            <p className={styles.text}>{email}</p>
+            <h3 className={styles.title}>Telefone</h3>
+            <p className={styles.text}>{phone}</p>
+            <h3 className={styles.title}>CPF</h3>
+            <p className={styles.text}>{cpf}</p>
+            <h3 className={styles.title}>País</h3>
+            {country?.map((item, i)=>(
+                <p key={i} className={styles.text}>{item}</p>
+            ))}
+            <h3 className={styles.title}>Cidade</h3>
+            {city?.map((item, i)=>(
+                <p key={i} className={styles.text}>{item}</p>
+            ))}
+        </div>
+    )
+}
